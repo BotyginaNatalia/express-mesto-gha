ebc701891b834b6cdd2b37a21159d8a77fcac49c
Natalia <n.botygina@mail.ru>
2022-07-31 12:57:19 +0300

    changes after 1st review

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,7 +19,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-template-curly-in-string, no-console
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
