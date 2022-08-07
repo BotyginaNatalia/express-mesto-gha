6914a99ed237f0141535df0866a1e5badce31a64
Natalia <n.botygina@mail.ru>
2022-08-07 19:34:20 +0300

    according to 14 project

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,18 @@
 const jwt = require('jsonwebtoken');
-const { NotFoundErr } = require('../errors/NotFoundErr');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(NotFoundErr).send('Необходима авторизация пользователя');
+    res.status(Error).send('Необходима авторизация пользователя');
+    return;
   }
-
   const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (error) {
-    return res.status(NotFoundErr).send('Необходима авторизация пользователя');
+    res.status(Error).send('Необходима авторизация');
+    return;
   }
   req.user = payload;
   next();
